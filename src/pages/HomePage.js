847b34d94dae7384df0066dc4d8726b3f09f9eea
theisaachome <65864160+theisaachome@users.noreply.github.com>
2022-01-29 15:18:54 +0630

    hero section component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { ProductConsumer } from "../context";
+import { Link } from 'react-router-dom';
+import Hero from "../components/Hero";
+
 const HomePage = () => {
-  return (<ProductConsumer>
-    {value=>{
-      return <h2>Home Page</h2>
-    }}
-  </ProductConsumer>);
+  return (
+    <>
+      <Hero title="Awesome Gadgets" max="true">
+        <Link style={{margin:"2rem"}} className='main-link' to="/products">our products</Link>
+      </Hero>
+    </>
+  );
 };
 
 export default HomePage;
